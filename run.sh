9066e97c5146489817c520d0c7107c49104f3459
adriannejulie <adriannejulia.lat@ucalgary.ca>
2024-03-27 20:22:39 -0600

    fixed

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ sleep 20  # Adjust the sleep duration as necessary
 
 # Start the React frontend
 echo "Starting React frontend..."
-cd ../fron_tend/REACT-APP  # Navigate to the frontend directory
+cd ../front_end/REACT-APP  # Navigate to the frontend directory
 npm install  # Install frontend dependencies
 npm start &  # Start the React development server in the background
 FRONTEND_PID=$!  # Save the process ID of the frontend
